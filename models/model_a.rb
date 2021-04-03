9648c407307b938fbc079b1dec9a3e4472b4b2b9
Ted Staros <tstaros23@gmail.com>
2021-04-02 21:24:28 -0600

    Add hash to model a dinosaur

---
+++ models/model_a.rb
@@ -0,0 +1,11 @@
+DINOSAUR = {
+  "first_name" => "Terry",
+  "last_name" => "Terrible"
+  "type_of_dinosaur" => "T Rex",
+  "his_height_in_feet" => 6,
+  "his_length_in_feet" => 6.2,
+  "dinosaur_having_fun" => false,
+  "dinosaur_is_mad" => true,
+  "collection_of_dinosaur_eggs" => ['blue','green','red'],
+  "his_sworn_enemies" => ['Ralph','Jim','Bob']
+}
