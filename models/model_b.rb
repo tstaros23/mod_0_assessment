bc6f1d52307e5562cfb232de9e6573045a2f5be3
Ted Staros <tstaros23@gmail.com>
2021-04-02 21:37:35 -0600

    Add hash to model a developer

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+DEVELOPER = {
+  "first_name" => "Ted",
+  "last_name" => "Staros",
+  "developer_height_in_feet" => 6,
+  "developer_width_in_feet" => 2.2,
+  "developer_expert_skill_level" => false,
+  "developer_beginner_skill_level" => true,
+  "developer_coding_languages_interested_in" => ['Java','Ruby','HTML'],
+  "developer_favorite_colors" => ['Blue','Red','Green'],
+}
