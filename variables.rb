3af2ee19ea915ced705208cb37df7c9e25e91a76
Ted Staros <tstaros23@gmail.com>
2021-04-02 21:01:36 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,9 @@
+movie = "Goodfellas";
+is_entertaining = true;
+movie = "Casino";
+num_4 = 4;
+num_6 = 6;
+product = num_6 * num_4;
+favorite_foods = ["Hotdogs","Burgers", "Fries"];
+print favorite_foods.shift(1);
+puts movie.count;
